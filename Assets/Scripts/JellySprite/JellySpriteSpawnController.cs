3db4f92a929ed82cbbf7c4e575b2937174e694d4
David Dionisio <ddionisio@renegadeware.com>
2019-04-14 19:56:23 -0400

    testing stuff out

---
+++ Assets/Scripts/JellySprite/JellySpriteSpawnController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellySpriteSpawnController : MonoBehaviour, M8.IPoolSpawn, M8.IPoolDespawn {
+    public const string parmPosition = "position";
+    public const string parmRotation = "rotation";
+    public const string parmSprite = "sprite";
+
+    public UnityJellySprite jellySprite;
+
+    private int mDefaultLayer;
+
+    void M8.IPoolSpawn.OnSpawned(M8.GenericParams parms) {
+
+    }
+
+    void M8.IPoolDespawn.OnDespawned() {
+
+    }
+
+    void Awake() {
+        
+    }
+}
